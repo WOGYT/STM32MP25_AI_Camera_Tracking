c3cabb05660789d758ab80962b973202f6f9a753
Gabriel <gabriel.legout9@gmail.com>
2024-11-20 16:59:24 +0100

    changes in main file

--- ai_cam.py
+++ ai_cam.py
@@ -1,12 +1,112 @@
 import cv2
 import urllib.request
+import gi
 import numpy as np
+
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst
 import time
 #preferred version == 3.6.3
 # this is working on 3.6.3  output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()] with CV 2
 # this "SHOULD" work in python ==3.8 and higher [layer_name for layer_name in net.getUnconnectedOutLayersNames()]
 
-url='http://192.168.1.23/jpg'    # base IP http://192.168.0.39/cam-hi.jpg
+#from patrickelectric video_udp.py repo
+
+class Video():
+    """BlueRov video capture class constructor
+    Attributes:
+        port (int): Video UDP port
+        video_codec (string): Source h264 parser
+        video_decode (string): Transform YUV (12bits) to BGR (24bits)
+        video_pipe (object): GStreamer top-level pipeline
+        video_sink (object): Gstreamer sink element
+        video_sink_conf (string): Sink configuration
+        video_source (string): Udp source ip and port
+    """
+
+    def __init__(self, port=5000):
+        """Summary
+        Args:
+            port (int, optional): UDP port
+        """
+
+        Gst.init(None)
+
+        self.port = port
+        self._frame = None
+
+        # [Software component diagram](https://www.ardusub.com/software/components.html)
+        # UDP video stream (:5600)
+        self.video_source = 'udpsrc port={}'.format(self.port)
+        # [Rasp raw image](http://picamera.readthedocs.io/en/release-0.7/recipes2.html#raw-image-capture-yuv-format)
+        # Cam -> CSI-2 -> H264 Raw (YUV 4-4-4 (12bits) I420)
+        self.video_codec = '! application/x-rtp, payload=96 ! rtph264depay ! h264parse ! avdec_h264'
+        # Python don't have nibble, convert YUV nibbles (4-4-4) to OpenCV standard BGR bytes (8-8-8)
+        self.video_decode = \
+            '! decodebin ! videoconvert ! video/x-raw,format=(string)BGR ! videoconvert'
+        # Create a sink to get data
+        self.video_sink_conf = \
+            '! appsink emit-signals=true sync=false max-buffers=2 drop=true'
+
+        self.video_pipe = None
+        self.video_sink = None
+
+        self.run()
+
+    @staticmethod
+    def gst_to_opencv(sample):
+        """Transform byte array into np array
+        Args:
+            sample (TYPE): Description
+        Returns:
+            TYPE: Description
+        """
+        buf = sample.get_buffer()
+        caps = sample.get_caps()
+        array = np.ndarray(
+            (
+                caps.get_structure(0).get_value('height'),
+                caps.get_structure(0).get_value('width'),
+                3
+            ),
+            buffer=buf.extract_dup(0, buf.get_size()), dtype=np.uint8)
+        return array
+
+    def frame(self):
+        """ Get Frame
+        Returns:
+            iterable: bool and image frame, cap.read() output
+        """
+        return self._frame
+
+    def frame_available(self):
+        """Check if frame is available
+        Returns:
+            bool: true if frame is available
+        """
+        return type(self._frame) != type(None)
+
+    def run(self):
+        """ Get frame to update _frame
+        """
+
+        self.start_gst(
+            [
+                self.video_source,
+                self.video_codec,
+                self.video_decode,
+                self.video_sink_conf
+            ])
+
+        self.video_sink.connect('new-sample', self.callback)
+
+    def callback(self, sink):
+        sample = sink.emit('pull-sample')
+        new_frame = self.gst_to_opencv(sample)
+        self._frame = new_frame
+
+        return Gst.FlowReturn.OK
+
 
 # Load the YOLO model
 net = cv2.dnn.readNet("./weights/yolov3.weights", "./configuration/yolov3.cfg")
@@ -23,10 +123,13 @@ starting_time = time.time()
 frame_id = 0
 labelv = 0
 
+video = Video()
+
 while True:
     # Read webcam
-    imgResp = urllib.request.urlopen(url)
-    imgNp = np.array(bytearray(imgResp.read()), dtype=np.uint8)
+    if not video.frame_available():
+        continue
+    imgNp = video.frame()
     frame = cv2.imdecode(imgNp, -1)
     frame_id += 1
     height, width, channels = frame.shape
